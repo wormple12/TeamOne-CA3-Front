b7fbd3a2374a2e05327d14d6d911b472ac435a87
unknown <doublenegative@protonmail.com>
2019-11-12 12:24:13 +0100

    made skeleton for hooks setup. Created empty settings file. Added files from security javascript project

--- ca3quickstart/src/App.js
+++ ca3quickstart/src/App.js
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from "react";
 
-function App() {
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams,
+  NavLink
+} from "react-router-dom";
+import facade from "./apiFacade";
+import "./App.css";
+import uuid from "uuid/v1";
+
+function App({ loginFunctionality }) {
+
+ const 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/userpage">
+          <UserPage props = />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
+const Login = ({ props }) => {
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <h2>Login</h2>
+      <form onSubmit={this.login} onChange={this.onChange}>
+        <input placeholder="User Name" id="username" />
+        <input placeholder="Password" id="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+const UserPage = ({ props }) => {
+  return(
+	  <div>
+
+	  </div>
+  );
+};
+
+const NoMatch = () => {
+  return <h3>Nothing here but us lemmings</h3>;
+};
+
+class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" };
+  }
+  login = evt => {
+    evt.preventDefault();
+    this.props.login(this.state.username, this.state.password);
+  };
+  onChange = evt => {
+    this.setState({ [evt.target.id]: evt.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        <h2>Login</h2>
+        <form onSubmit={this.login} onChange={this.onChange}>
+          <input placeholder="User Name" id="username" />
+          <input placeholder="Password" id="password" />
+          <button>Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataFromServer: "Fetching!!" };
+  }
+  componentDidMount() {}
+  render() {
+    return (
+      <div>
+        <h2>Data Received from server</h2>
+        <h3>{this.state.dataFromServer}</h3>
+      </div>
+    );
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  }; //TODO
+  login = (user, pass) => {
+    facade.login(user, pass).then(res => this.setState({ loggedIn: true }));
+  }; //TODO
+  render() {
+    return (
+      <div>
+        {!this.state.loggedIn ? (
+          <LogIn login={this.login} />
+        ) : (
+          <div>
+            <LoggedIn />
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 export default App;
