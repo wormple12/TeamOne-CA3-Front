24a22165d90c0be026788f97b5e68383b736bee6
unknown <doublenegative@protonmail.com>
2019-11-15 16:53:29 +0100

    fixed table. Again

--- ca3quickstart/src/utils.js
+++ ca3quickstart/src/utils.js
@@ -3,154 +3,180 @@ import uuid from "uuid/v1";
 import React from "react";
 
 export function catchHttpErrors(err) {
-  if (err.status) {
-    err.fullError.then(e => console.log(e.detail));
-  } else {
-    console.log("Network error");
-  }
+	if (err.status) {
+		err.fullError.then(e => console.log(e.detail));
+	} else {
+		console.log("Network error");
+	}
 }
 
 export function handleHttpErrors(res) {
-  if (!res.ok) {
-    return Promise.reject({ status: res.status, fullError: res.json() });
-  }
-  return res.json();
+	if (!res.ok) {
+		return Promise.reject({ status: res.status, fullError: res.json() });
+	}
+	return res.json();
 }
 
 export const makeOptions = (method, addToken, body) => {
-  var opts = {
-    method: method,
-    headers: {
-      "Content-type": "application/json",
-      Accept: "application/json"
-    }
-  };
-  if (addToken && loginFacade.loggedIn()) {
-    opts.headers["x-access-token"] = loginFacade.getToken();
-  }
-  if (body) {
-    opts.body = JSON.stringify(body);
-  }
-  return opts;
+	var opts = {
+		method: method,
+		headers: {
+			"Content-type": "application/json",
+			Accept: "application/json"
+		}
+	};
+	if (addToken && loginFacade.loggedIn()) {
+		opts.headers["x-access-token"] = loginFacade.getToken();
+	}
+	if (body) {
+		opts.body = JSON.stringify(body);
+	}
+	return opts;
 };
 
 const tableTime = (function() {
-  function embeddedTableCreation(info) {
-    let filtered = { ...info };
-    let noObjects = Object.entries(filtered).filter(
-      o => typeof o[1] !== "object"
-    );
-    return (
-      <div>
-        <h3>Person</h3>
-        <table className="table">
-          <thead>
-            <tr>
-              {Object.values(noObjects).map(j => (
-                <th key={uuid()}>{j[0]}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              {Object.values(noObjects).map(j => (
-                <td key={uuid()}>{j[1]}</td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
-        <br></br>
-        {multiTable2(info)}
-      </div>
-    );
-  }
+	function embeddedTableCreation(info) {
+		let filtered = { ...info };
+		console.log(filtered);
+		filtered = Object.entries(filtered).filter(
+			o => !(Object.values(o[1]).length <= 0)
+		);
+		console.log("filtered+", filtered);
 
-  function multiTable(info) {
-    let d = Object.entries({ ...info });
-    let kd = d.filter(o => typeof Object.values(o)[1] === "object");
-    let kdr = kd.filter(
-      o =>
-        typeof Object.values(Object.values(o))[1][Symbol.iterator] !==
-        "function"
-    );
+		let restructured = {};
 
-    return (
-      <div>
-        {kdr.map(o => (
-          <div key={uuid()}>
-            <h3>{Object.values(o[0])}</h3>
-            <table className="table">
-              <thead>
-                <tr>
-                  {Object.keys(o[1]).map(j => (
-                    <th key={uuid()}>{j}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  {Object.values(o[1]).map(j => (
-                    <td key={uuid()}>{j}</td>
-                  ))}
-                </tr>
-              </tbody>
-            </table>
-            <br></br>
-          </div>
-        ))}
-      </div>
-    );
-  }
+		//filtered.map(arr => {
+		//	restructured.push(
+		//		Object.assign(Object.values(arr)[0], Object.values(arr)[1])
+		//	);
+		//});
 
-  function multiTable2(info) {
-    let d = Object.entries({ ...info });
-    let kd = d.filter(o => typeof Object.values(o)[1] === "object");
-    let kdr = kd.filter(
-      o =>
-        typeof Object.values(Object.values(o))[1][Symbol.iterator] ===
-        "function"
-    );
-    let kdrjl = kdr.map(o => Object.values(Object.values(o)));
-    let kdrjll = kdrjl.map(o => Object.values(o)[1]);
-    let count = 0;
+		filtered.map(arr => {
+			restructured[Object.values(arr)[0]] = Object.values(arr)[1];
+		});
+		console.log("restructured:", restructured);
 
-    return (
-      <div>
-        {multiTable(info)}
-        {kdrjll.map(o => (
-          <div key={uuid()}>
-            <h3>{Object.values(kdrjl[count++][0])}</h3>
+		let noObjects = Object.entries(restructured).filter(
+			o => typeof o[1] !== "object"
+		);
 
-            <table className="table">
-              <thead>
-                <tr>
-                  {Object.keys(Object.values(o)[0]).map(k => (
-                    <th key={uuid()}>{k}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {Object.values(o).map(j => (
-                  <tr key={uuid()}>
-                    {Object.values(j).map(k => (
-                      <td key={uuid()}>{k}</td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <br></br>
-          </div>
-        ))}
-      </div>
-    );
-  }
+		return (
+			<div>
+				<h3>Person</h3>
+				<table className="table">
+					<thead>
+						<tr>
+							{Object.values(noObjects).map(j => (
+								<th key={uuid()}>{j[0]}</th>
+							))}
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							{Object.values(noObjects).map(j => (
+								<td key={uuid()}>{j[1]}</td>
+							))}
+						</tr>
+					</tbody>
+				</table>
+				<br></br>
+				{multiTable2(restructured)}
+			</div>
+		);
+	}
 
-  return {
-    embeddedTableCreation: embeddedTableCreation,
-    multiTable: multiTable,
-    multiTable2: multiTable2
-  };
+	function multiTable(info) {
+		let d = Object.entries({ ...info });
+		//console.log(d);
+		//Object.values(d).map(o => console.log(o));
+		//Object.values(d).map(o => console.log(o[1].length));
+		//d = Object.values(d).filter(o => !(o[1].length <= 0));
+		//console.log(d);
+
+		let kd = d.filter(o => typeof Object.values(o)[1] === "object");
+		let kdr = kd.filter(
+			o =>
+				typeof Object.values(Object.values(o))[1][Symbol.iterator] !==
+				"function"
+		);
+
+		return (
+			<div>
+				{kdr.map(o => (
+					<div key={uuid()}>
+						<h3>{Object.values(o[0])}</h3>
+						<table className="table">
+							<thead>
+								<tr>
+									{Object.keys(o[1]).map(j => (
+										<th key={uuid()}>{j}</th>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									{Object.values(o[1]).map(j => (
+										<td key={uuid()}>{j}</td>
+									))}
+								</tr>
+							</tbody>
+						</table>
+						<br></br>
+					</div>
+				))}
+			</div>
+		);
+	}
+
+	function multiTable2(info) {
+		let d = Object.entries({ ...info });
+		let kd = d.filter(o => typeof Object.values(o)[1] === "object");
+		let kdr = kd.filter(
+			o =>
+				typeof Object.values(Object.values(o))[1][Symbol.iterator] ===
+				"function"
+		);
+		let kdrjl = kdr.map(o => Object.values(Object.values(o)));
+		let kdrjll = kdrjl.map(o => Object.values(o)[1]);
+		let count = 0;
+
+		return (
+			<div>
+				{multiTable(info)}
+				{kdrjll.map(o => (
+					<div key={uuid()}>
+						<h3>{Object.values(kdrjl[count++][0])}</h3>
+
+						<table className="table">
+							<thead>
+								<tr>
+									{Object.keys(Object.values(o)[0]).map(k => (
+										<th key={uuid()}>{k}</th>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								{Object.values(o).map(j => (
+									<tr key={uuid()}>
+										{Object.values(j).map(k => (
+											<td key={uuid()}>{k}</td>
+										))}
+									</tr>
+								))}
+							</tbody>
+						</table>
+						<br></br>
+					</div>
+				))}
+			</div>
+		);
+	}
+
+	return {
+		embeddedTableCreation: embeddedTableCreation,
+		multiTable: multiTable,
+		multiTable2: multiTable2
+	};
 })();
 
 export default tableTime;
