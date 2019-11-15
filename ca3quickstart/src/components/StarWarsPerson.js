d243b1ee0686c852ffd393ca380eef5f91885d01
wormple12 <wormple12@hotmail.com>
2019-11-15 13:39:55 +0100

    StarWarsPerson component is now in its own js file

---
+++ ca3quickstart/src/components/StarWarsPerson.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { catchHttpErrors } from "../utils";
+import { Redirect } from "react-router-dom";
+
+const StarWarsPerson = ({ loggedIn, starFacade, starInfo, setStarInfo }) => {
+  useEffect(() => {
+    starFacade
+      .FetchStar(1)
+      .then(d => setStarInfo(starFacade.MultiTable2(d)))
+      .catch(o => {
+        catchHttpErrors(o);
+      });
+  }, []);
+
+  if (!loggedIn) {
+    return <Redirect to={"/loggedOut"} />;
+  } else {
+    return <div>{starInfo}</div>;
+  }
+};
+
+export default StarWarsPerson;
--- ca3quickstart/src/starFacade.js
+++ ca3quickstart/src/starFacade.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
-	HashRouter as Router,
-	Switch,
-	Route,
-	useRouteMatch,
-	useParams
+  HashRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams
 } from "react-router-dom";
 import "./App.css";
 import LogIn from "./components/LogIn";
@@ -15,118 +15,118 @@ import apiFacade from "./loginFacade";
 import { object } from "prop-types";
 
 const starFacade = (function() {
-	function FetchStar(id) {
-		const options = makeOptions("GET", true);
-		return fetch(
-			configuration.URL + "/api/starwars/starWars/" + id,
-			options
-		).then(o => o.json());
-	}
+  function FetchStar(id) {
+    const options = makeOptions("GET", true);
+    return fetch(
+      configuration.URL + "/api/starwars/starWars/" + id,
+      options
+    ).then(o => o.json()); // handleHTTPErrors
+  }
 
-	function ImbeddedTableCreation(info) {
-		let filtered = { ...info };
-		let noObjects = Object.entries(filtered).filter(
-			o => typeof o[1] !== "object"
-		);
-		return (
-			<div>
-				<table className="table">
-					<thead>
-						<tr>
-							{Object.values(noObjects).map(j => (
-								<th>{j[0]}</th>
-							))}
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							{Object.values(noObjects).map(j => (
-								<td>{j[1]}</td>
-							))}
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		);
-	}
+  function ImbeddedTableCreation(info) {
+    let filtered = { ...info };
+    let noObjects = Object.entries(filtered).filter(
+      o => typeof o[1] !== "object"
+    );
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.values(noObjects).map(j => (
+                <th>{j[0]}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {Object.values(noObjects).map(j => (
+                <td>{j[1]}</td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
-	function MultiTable(info) {
-		let d = Object.entries({ ...info });
-		let kd = d.filter(o => typeof Object.values(o)[1] === "object");
-		let kdr = kd.filter(
-			o =>
-				typeof Object.values(Object.values(o))[1][Symbol.iterator] !==
-				"function"
-		);
-		return (
-			<div>
-				{kdr.map(o => (
-					<table className="table">
-						<thead>
-							<tr>
-								{Object.keys(o[1]).map(j => (
-									<th>{j}</th>
-								))}
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								{Object.values(o[1]).map(j => (
-									<td>{j}</td>
-								))}
-							</tr>
-						</tbody>
-					</table>
-				))}
-			</div>
-		);
-	}
+  function MultiTable(info) {
+    let d = Object.entries({ ...info });
+    let kd = d.filter(o => typeof Object.values(o)[1] === "object");
+    let kdr = kd.filter(
+      o =>
+        typeof Object.values(Object.values(o))[1][Symbol.iterator] !==
+        "function"
+    );
+    return (
+      <div>
+        {kdr.map(o => (
+          <table className="table">
+            <thead>
+              <tr>
+                {Object.keys(o[1]).map(j => (
+                  <th>{j}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {Object.values(o[1]).map(j => (
+                  <td>{j}</td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        ))}
+      </div>
+    );
+  }
 
-	function MultiTable2(info) {
-		let d = Object.entries({ ...info });
-		let kd = d.filter(o => typeof Object.values(o)[1] === "object");
-		let kdr = kd.filter(
-			o =>
-				typeof Object.values(Object.values(o))[1][Symbol.iterator] ===
-				"function"
-		);
-		let kdrjl = kdr.map(o => Object.values(Object.values(o)));
-		let kdrjll = kdrjl.map(o => Object.values(o)[1]);
+  function MultiTable2(info) {
+    let d = Object.entries({ ...info });
+    let kd = d.filter(o => typeof Object.values(o)[1] === "object");
+    let kdr = kd.filter(
+      o =>
+        typeof Object.values(Object.values(o))[1][Symbol.iterator] ===
+        "function"
+    );
+    let kdrjl = kdr.map(o => Object.values(Object.values(o)));
+    let kdrjll = kdrjl.map(o => Object.values(o)[1]);
 
-		return (
-			<div>
-				{MultiTable(info)}
-				{kdrjll.map(o => (
-					<table className="table">
-						<thead>
-							<tr>
-								{Object.keys(Object.values(o)[0]).map(k => (
-									<th>{k}</th>
-								))}
-							</tr>
-						</thead>
-						<tbody>
-							{Object.values(o).map(j => (
-								<tr>
-									{Object.values(j).map(k => (
-										<td>{k}</td>
-									))}
-								</tr>
-							))}
-						</tbody>
-						<br></br>
-					</table>
-				))}
-			</div>
-		);
-	}
+    return (
+      <div>
+        {MultiTable(info)}
+        {kdrjll.map(o => (
+          <table className="table">
+            <thead>
+              <tr>
+                {Object.keys(Object.values(o)[0]).map(k => (
+                  <th>{k}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {Object.values(o).map(j => (
+                <tr>
+                  {Object.values(j).map(k => (
+                    <td>{k}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+            <br></br>
+          </table>
+        ))}
+      </div>
+    );
+  }
 
-	return {
-		FetchStar: FetchStar,
-		ImbeddedTableCreation: ImbeddedTableCreation,
-		MultiTable: MultiTable,
-		MultiTable2: MultiTable2
-	};
+  return {
+    FetchStar: FetchStar,
+    ImbeddedTableCreation: ImbeddedTableCreation,
+    MultiTable: MultiTable,
+    MultiTable2: MultiTable2
+  };
 })();
 
 export default starFacade;
