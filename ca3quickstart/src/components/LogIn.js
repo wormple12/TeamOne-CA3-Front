9d4d5ddf56f277efcd135fe5b3e1cf86a2a37296
wormple12 <wormple12@hotmail.com>
2019-11-15 13:07:25 +0100

    Star Wars table doesn't visibly load every time, the page is accessed

--- ca3quickstart/src/components/LogIn.js
+++ ca3quickstart/src/components/LogIn.js
@@ -3,84 +3,80 @@ import { catchHttpErrors, makeOptions } from "../utils";
 import configuration from "../settings";
 import { handleHttpErrors } from "../utils";
 
-const LogIn = ({ apiFacade, loggedIn, setLoggedIn, starFacade }) => {
-	const logout = () => {
-		apiFacade.logout();
-		setLoggedIn(false);
-	};
-	const login = (user, pass) => {
-		apiFacade
-			.login(user, pass)
-			.then(res => setLoggedIn(true))
-			.catch(err => {
-				console.log(err);
-				err.fullError.then(function(result) {
-					alert(result.message);
-				});
-				catchHttpErrors(err);
-			});
-	};
-	return (
-		<div>
-			{!loggedIn ? (
-				<LogInForm login={login} />
-			) : (
-				<LoggedIn
-					apiFacade={apiFacade}
-					logout={logout}
-					starFacade={starFacade}
-				/>
-			)}
-		</div>
-	);
+const LogIn = ({ apiFacade, loggedIn, setLoggedIn }) => {
+  const logout = () => {
+    apiFacade.logout();
+    setLoggedIn(false);
+  };
+  const login = (user, pass) => {
+    apiFacade
+      .login(user, pass)
+      .then(res => setLoggedIn(true))
+      .catch(err => {
+        console.log(err);
+        err.fullError.then(function(result) {
+          alert(result.message);
+        });
+        catchHttpErrors(err);
+      });
+  };
+  return (
+    <div>
+      {!loggedIn ? (
+        <LogInForm login={login} />
+      ) : (
+        <LoggedIn apiFacade={apiFacade} logout={logout} />
+      )}
+    </div>
+  );
 };
 
 export default LogIn;
 
 const LogInForm = ({ login }) => {
-	const emptyUser = { username: "", password: "" };
-	const [user, setUser] = useState(emptyUser);
+  const emptyUser = { username: "", password: "" };
+  const [user, setUser] = useState(emptyUser);
 
-	const handleChange = event => {
-		event.preventDefault();
-		setUser({ ...user, [event.target.id]: event.target.value });
-	};
+  const handleChange = event => {
+    event.preventDefault();
+    setUser({ ...user, [event.target.id]: event.target.value });
+  };
 
-	const handleSubmit = event => {
-		event.preventDefault();
-		console.log("Submission..");
-		login(user.username, user.password);
-	};
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("Submission..");
+    login(user.username, user.password);
+  };
 
-	return (
-		<div>
-			<h2>Login</h2>
-			<form onSubmit={handleSubmit} onChange={handleChange}>
-				<input placeholder="User Name" id="username" />
-				<input placeholder="Password" id="password" />
-				<button>Login</button>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} onChange={handleChange}>
+        <input placeholder="User Name" id="username" />
+        <input placeholder="Password" id="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 const LoggedIn = ({ apiFacade, logout }) => {
-	const options = makeOptions("GET", true);
-	const [data, setData] = useState("Fetching");
-	const user = fetch(
-		configuration.URL + "/api/starwars/" + apiFacade.tokenDecoder().username,
-		options
-	)
-		.then(handleHttpErrors)
-		.then(data => {
-			setData(data.msg);
-		});
+  const options = makeOptions("GET", true);
+  const [data, setData] = useState("Fetching");
+  const user = fetch(
+    configuration.URL + "/api/starwars/" + apiFacade.tokenDecoder().username,
+    options
+  )
+    .then(handleHttpErrors)
+    .then(data => {
+      setData(data.msg);
+    });
 
-	return (
-		<div>
-			<h2>Data Received from server</h2>
-			<h3>{data}</h3>
-			<button onClick={logout}>Logout</button>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Data Received from server</h2>
+      <h3>{data}</h3>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 };
